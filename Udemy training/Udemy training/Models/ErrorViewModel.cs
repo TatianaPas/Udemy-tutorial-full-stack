dd66daa9f22f8dac6c5bb04683c39b5837738c08
Tatiana <tatjana.pasechnik@gmail.com>
2022-10-06 12:31:39 +1300

    End of section 2

--- Udemy training/Udemy training/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Udemy_training.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ Udemy training/WebApiUdemy/Models/Domain/Walk.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApiUdemy.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkdifficultyId { get; set; }
+
+        //Navigation properties
+
+        public Region Region { get; set; }
+        public WalkDifficulty walkDifficulty { get; set; }
+
+    }
+}
